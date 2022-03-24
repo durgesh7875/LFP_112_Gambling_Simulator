735549341eccad5bc7f3f0115e2d4de1db3786e1
Durgesh094 <durgeshchavan1994@gmail.com>
2022-03-25 00:00:55 +0530

    Successfully check for luckiest day maximum win or unluckiest day maximum lost.

--- src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -2,28 +2,68 @@ package com.bridgelabz.gamblingsimulator;
 
 public class GamblingSimulator {
 
+    public static int counter;
     static int BET_PER_DAY = 1;
     static int TOTAL_MONTHS = 5;
-    static int counter;
     static int TOTAL_DAYS = 20;
-    static int[] perDayAmount; //for the storing day wize amount
+    static int[] perDayAmount;// = new int[TOTAL_DAYS];//for the storing day wiz amount
 
     GamblingSimulator() {
-        counter = 0; //initialize the counter
+
+        counter = 0;//initialize the counter
     }
 
-    // check bet win or loose
-    public static void checkWinOrLoose() {
+    public static void main(String[] args) {
         Stakes stakes = new Stakes();
+        while (TOTAL_MONTHS >= 1) {
+            TOTAL_DAYS = 20;
+            counter = 0;
+            perDayAmount = new int[TOTAL_DAYS];
+            int totalDays = TOTAL_DAYS;
+            while (totalDays > 0) {
+                checkWinnerLoose();
+                totalDays--;
+            }
+            TOTAL_MONTHS--;
+            calculateLuckyDay(perDayAmount);
+        }
+    }
 
-        int looseCash = 0;
+    public static void calculateLuckyDay(int[] perDayAmount) {
+        int lucky = perDayAmount[0];
+        int unLucky = perDayAmount[0];
+        int luckyWinDay = 0;
+        int unLuckyLostDay = 0;
+        for (int i = 0; i < perDayAmount.length; i++) {
+            if (lucky < perDayAmount[i]) {
+                luckyWinDay = i;
+                lucky = perDayAmount[i];
+            }
+            if (unLucky > perDayAmount[i]) {
+                unLuckyLostDay = i;
+                unLucky = perDayAmount[i];
+            }
+        }
+        System.out.println("luckiest day where you won maximum : " + luckyWinDay);
+        System.out.println("Unluckiest day where you lost maximum : " + unLuckyLostDay);
+    }
+
+    //Print the day wise amount
+    public static void printResult() {
+        for (int i = 0; i < TOTAL_DAYS; i++) {
+            System.out.println("Day " + (i + 1) + " Amount is " + perDayAmount[i]);
+        }
+    }
+
+    //Check the bet win or loose
+    public static void checkWinnerLoose() {
+        Stakes stakes = new Stakes();
+        int loosCash = 0;
         int winCash = 0;
-
-        // 50% of stake
+        //50% of Stake
         int minStake = stakes.getTotalStakes() - 50;
         int maxStake = stakes.getTotalStakes() + 50;
-
-        // win or loose 50% stake
+        //Win lost 50% stake
         while (stakes.getTotalStakes() > minStake && stakes.getTotalStakes() < maxStake) {
             int betState = (int) ((Math.random() * 10)) % 2;
 
@@ -32,47 +72,20 @@ public class GamblingSimulator {
             if (betState == 1) {
                 stakes.setTotalStakes(bet + BET_PER_DAY);
                 winCash++;
+
             } else {
                 stakes.setTotalStakes(bet - BET_PER_DAY);
-                looseCash++;
+                loosCash++;
             }
         }
-
-        calculatePerDayAmount(stakes.getTotalStakes(), winCash, looseCash);
+        calculatePerDayAmmount(stakes.getTotalStakes(), winCash, loosCash);
     }
 
-    public static void calculatePerDayAmount(int totalStakes, int winCash, int looseCash) {
-        // storing win or loose amount in variable
+    public static void calculatePerDayAmmount(int totalStakes, int winCash, int loosCash) {
+        // Storing the win or lost amount in variable
         if (totalStakes == 150) {
             perDayAmount[counter++] = winCash;
-        } else {
-            perDayAmount[counter++] = looseCash;
-        }
-    }
-
-    // print day waise amount
-    public static void printResult() {
-        for (int i = 0; i < TOTAL_DAYS; i++)
-            System.out.println("Day " + (i + 1) + " --> " + "Amount is: " + perDayAmount[i] + "$");
-    }
-
-    public static void main(String[] args) {
-        Stakes stakes = new Stakes();
-
-        while (TOTAL_MONTHS >= 1) {
-            TOTAL_DAYS = 20;
-            counter = 0;
-            perDayAmount = new int[TOTAL_DAYS];
-
-        int totalDays = TOTAL_DAYS;
-        while (totalDays > 0) {
-            checkWinOrLoose();
-            totalDays--;
-        }
-
-            printResult();
-        }
-        TOTAL_MONTHS--;
+        } else perDayAmount[counter++] = loosCash;
     }
 }
 
