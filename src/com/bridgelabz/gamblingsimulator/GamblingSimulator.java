508e113d27a6be7047bb5fc2082a23f01c8947e1
Durgesh094 <durgeshchavan1994@gmail.com>
2022-03-24 23:09:44 +0530

    Successfully calculate per day amount.

--- src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -3,37 +3,65 @@ package com.bridgelabz.gamblingsimulator;
 public class GamblingSimulator {
 
     static int BET_PER_DAY = 1;
+    static int counter;
+    static final int TOTAL_DAYS = 20;
+    static int[] perDayAmount = new int[TOTAL_DAYS]; //for the storing day wize amount
 
+    GamblingSimulator() {
+        counter = 0; //initialize the counter
+    }
+
+    // check bet win or loose
     public static void checkWinOrLoose() {
         Stakes stakes = new Stakes();
 
+        int looseCash = 0;
+        int winCash = 0;
+
+        // 50% of stake
         int minStake = stakes.getTotalStakes() - 50;
         int maxStake = stakes.getTotalStakes() + 50;
 
-        while(stakes.getTotalStakes() > minStake && stakes.getTotalStakes() > maxStake) {
+        // win or loose 50% stake
+        while (stakes.getTotalStakes() > minStake && stakes.getTotalStakes() < maxStake) {
             int betState = (int) ((Math.random() * 10)) % 2;
 
             int bet = stakes.getTotalStakes();
 
-            if(betState == 1) {
+            if (betState == 1) {
                 stakes.setTotalStakes(bet + BET_PER_DAY);
-            }
-            else {
+                winCash++;
+            } else {
                 stakes.setTotalStakes(bet - BET_PER_DAY);
+                looseCash++;
             }
         }
+
+        calculatePerDayAmount(stakes.getTotalStakes(), winCash, looseCash);
     }
 
+    public static void calculatePerDayAmount (int totalStakes, int winCash, int looseCash) {
+        // storing win or loose amount in variable
+        if(totalStakes == 150) {
+            perDayAmount[counter++] = winCash;
+        }
+        else {
+            perDayAmount[counter++] = looseCash;
+        }
+    }
+    // print day waise amount
     public static void printResult () {
-        Stakes stakes = new Stakes();
-
-        // Start with 100$ Stakes for every day and 1$ bet for every game.
-        System.out.println("Total stakes for day: " + stakes.getTotalStakes() + "$");
-        System.out.println("Total bet per game: " + stakes.getBetPerGame() + "$");
+        for (int i = 0; i < TOTAL_DAYS; i++)
+            System.out.println("Day " + (i+1) + " --> " + "Amount is: " + perDayAmount[i] + "$");
     }
 
     public static void main(String[] args) {
-        checkWinOrLoose();
+        Stakes stakes = new Stakes();
+        int totalDays = TOTAL_DAYS;
+        while (totalDays > 0) {
+            checkWinOrLoose();
+            totalDays--;
+        }
         printResult();
     }
 }
